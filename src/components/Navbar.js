3c36e3c25e8a3731ab39cd9380184664c2f568ab
piyush-1871 <piyush1871b@gmail.com>
2022-10-13 13:39:26 +0530

    implementing the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from "react";
-
+import {Link} from 'react-router-dom'
 function Navbar() {
   return (
-    <div className="">
-      Navbar
+    <div>
+      <ul id="nav">
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/create-post'>Create Post</Link></li>
+      </ul>
     </div>
   );
 }
