492606de38294dd594ce266a312154074f33f802
piyush-1871 <piyush1871b@gmail.com>
2022-10-13 15:29:04 +0530

    created post details

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
+import { firestore } from "../firebase";
 
 function PostDetail() {
-    return (
-      <div className="">
-        PostDetail
+
+  const[post, setPost] = useState({});
+  const { postId } = useParams();
+  useEffect(()=>{
+    firestore
+      .collection('posts')
+      .doc(postId)
+      .get()
+      .then((snapsot)=>{
+        console.log('snapshot',snapsot.data());
+        setPost(snapsot.data());
+      });
+  },[]);
+
+  return (
+      <div className="post-detail">
+        <h1>{post.title}</h1>
+        <p>{post.content}</p>
       </div>
     );
   }
